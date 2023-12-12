91e96769629dd2cf615d99749e1bc3066090d6e8
hsyoodev <hsyoodev@gmail.com>
2023-12-12 13:43:47 +0900

    animation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,32 +1,21 @@
 package com.example.myapplication;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button button;
     ImageView imageView;
-    float rotate = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rotate += 10;
-                imageView.setRotation(rotate);
-            }
-        });
+        AnimationDrawable ani = (AnimationDrawable) imageView.getBackground();
+        ani.start();
     }
 }
