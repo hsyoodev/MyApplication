2918cc5be58f9bbcc02de74a12d31d207a14e54d
hsyoodev <hsyoodev@gmail.com>
2023-12-12 11:23:31 +0900

    4장 ì 연습문제 MagicEye

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,59 +1,51 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.ToggleButton;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    CheckBox android;
-    CheckBox apple;
-    ToggleButton toggleButton;
-    ImageView imageView;
+    RadioButton horse, car, pegasus;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android = findViewById(R.id.android);
-        apple = findViewById(R.id.apple);
-        toggleButton = findViewById(R.id.toggle);
-        imageView = findViewById(R.id.image);
+        horse = findViewById(R.id.horse);
+        car = findViewById(R.id.car);
+        pegasus = findViewById(R.id.pegasus);
+        image = findViewById(R.id.image);
 
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        horse.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.e("toggle", String.valueOf(isChecked));
                 if (isChecked) {
-                    imageView.setVisibility(View.VISIBLE);
-                    android.setChecked(true);
-                    apple.setChecked(true);
-                } else {
-                    imageView.setVisibility(View.GONE);
-                    android.setChecked(false);
-                    apple.setChecked(false);
+                    image.setImageResource(R.drawable.mg_1);
                 }
             }
         });
 
-        android.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        car.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.w("android", String.valueOf(isChecked));
+                if (isChecked) {
+                    image.setImageResource(R.drawable.mg_2);
+                }
             }
         });
 
-        apple.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        pegasus.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.w("apple", String.valueOf(isChecked));
+                if (isChecked) {
+                    image.setImageResource(R.drawable.mg_3);
+                }
             }
         });
     }
