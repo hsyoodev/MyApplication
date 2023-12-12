a545338aeb274b5d765a11303e3340e038187371
hsyoodev <hsyoodev@gmail.com>
2023-12-12 15:20:51 +0900

    레이아웃 배치 연습

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageView imageView = findViewById(R.id.bell);
-
-        Animation ani = AnimationUtils.loadAnimation(MainActivity.this, R.anim.shake);
-        imageView.startAnimation(ani);
     }
 }
