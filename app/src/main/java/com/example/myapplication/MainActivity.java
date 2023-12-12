a9f931ec2e31b39bf309bd85082e337165c2986f
hsyoodev <hsyoodev@gmail.com>
2023-12-12 10:33:26 +0900

    togglebutton

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     CheckBox android;
     CheckBox apple;
+    ToggleButton toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,21 @@ public class MainActivity extends AppCompatActivity {
 
         android = findViewById(R.id.android);
         apple = findViewById(R.id.apple);
+        toggleButton = findViewById(R.id.toggle);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.e("toggle", String.valueOf(isChecked));
+                if (isChecked) {
+                    android.setChecked(false);
+                    apple.setChecked(false);
+                } else {
+                    android.setChecked(true);
+                    apple.setChecked(true);
+                }
+            }
+        });
 
         android.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
