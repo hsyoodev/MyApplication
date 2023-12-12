7588767493dfaac38caf673a776870bbbc5efe51
hsyoodev <hsyoodev@gmail.com>
2023-12-12 10:08:57 +0900

    안드로이드 스튜디오에서 main ë©메서드를 실행하는 방법

---
+++ app/src/main/java/com/example/myapplication/Main.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("안드로이드 스튜디오에서 main 메서드를 실행하는 방법");
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,29 +1,13 @@
 package com.example.myapplication;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLConnection;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,57 +22,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.editText);
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        radioButton1 = findViewById(R.id.radioButton1);
-        radioButton2 = findViewById(R.id.radioButton2);
-        imageView = findViewById(R.id.imageView);
-
-        button1.setOnClickListener(view -> Toast.makeText(getApplicationContext(), editText.getText(), Toast.LENGTH_SHORT).show());
-        button2.setOnClickListener(view -> {
-            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com"));
-            startActivity(intent);
-        });
-
-        radioButton1.setOnClickListener(view -> imageView.setImageResource(R.drawable.snow_corn));
-        radioButton2.setOnClickListener(view -> imageView.setImageResource(R.drawable.tiramisu01));
-
-        ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("데이터를 가져오는 중");
-        progressDialog.setCancelable(false);
-        progressDialog.show();
-        Thread thread = new Thread(() -> {
-            try {
-                URI uri = new URI("http://ggoreb.com/api/toc.jsp");
-                URLConnection urlConnection = uri.toURL().openConnection();
-                BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-                while (true) {
-                    String data = br.readLine();
-                    if (Objects.isNull(data)) {
-                        break;
-                    }
-                    sb.append(data);
-                }
-                br.close();
-                ObjectMapper objectMapper = new ObjectMapper();
-                List<Map<String, Object>> tocs = objectMapper.readValue(sb.toString(), List.class);
-                runOnUiThread(() -> {
-                    for (Map<String, Object> toc : tocs) {
-                        editText.setText(toc.get("id").toString());
-                    }
-                });
-                progressDialog.dismiss();
-
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        thread.start();
+//        editText = findViewById(R.id.editText);
+//        button1 = findViewById(R.id.button1);
+//        button2 = findViewById(R.id.button2);
+//        radioButton1 = findViewById(R.id.radioButton1);
+//        radioButton2 = findViewById(R.id.radioButton2);
+//        imageView = findViewById(R.id.imageView);
+//
+//        button1.setOnClickListener(view -> Toast.makeText(getApplicationContext(), editText.getText(), Toast.LENGTH_SHORT).show());
+//        button2.setOnClickListener(view -> {
+//            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.naver.com"));
+//            startActivity(intent);
+//        });
+//
+//        radioButton1.setOnClickListener(view -> imageView.setImageResource(R.drawable.snow_corn));
+//        radioButton2.setOnClickListener(view -> imageView.setImageResource(R.drawable.tiramisu01));
+//
+//        ProgressDialog progressDialog = new ProgressDialog(this);
+//        progressDialog.setMessage("데이터를 가져오는 중");
+//        progressDialog.setCancelable(false);
+//        progressDialog.show();
+//        Thread thread = new Thread(() -> {
+//            try {
+//                URI uri = new URI("http://ggoreb.com/api/toc.jsp");
+//                URLConnection urlConnection = uri.toURL().openConnection();
+//                BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+//                StringBuilder sb = new StringBuilder();
+//                while (true) {
+//                    String data = br.readLine();
+//                    if (Objects.isNull(data)) {
+//                        break;
+//                    }
+//                    sb.append(data);
+//                }
+//                br.close();
+//                ObjectMapper objectMapper = new ObjectMapper();
+//                List<Map<String, Object>> tocs = objectMapper.readValue(sb.toString(), List.class);
+//                runOnUiThread(() -> {
+//                    for (Map<String, Object> toc : tocs) {
+//                        editText.setText(toc.get("id").toString());
+//                    }
+//                });
+//                progressDialog.dismiss();
+//
+//            } catch (URISyntaxException e) {
+//                throw new RuntimeException(e);
+//            } catch (MalformedURLException e) {
+//                throw new RuntimeException(e);
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+//        thread.start();
     }
 }
