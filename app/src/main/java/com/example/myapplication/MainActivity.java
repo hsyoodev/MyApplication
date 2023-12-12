fc30aec4c1621d7b5a980b535e84f5ddb742ea9f
hsyoodev <hsyoodev@gmail.com>
2023-12-12 12:17:54 +0900

    4장 연습문제 Rotate

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,51 +1,31 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.CompoundButton;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioButton horse, car, pegasus;
-    ImageView image;
+    Button button;
+    ImageView imageView;
+    float rotate = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        horse = findViewById(R.id.horse);
-        car = findViewById(R.id.car);
-        pegasus = findViewById(R.id.pegasus);
-        image = findViewById(R.id.image);
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
 
-        horse.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    image.setImageResource(R.drawable.mg_1);
-                }
-            }
-        });
-
-        car.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    image.setImageResource(R.drawable.mg_2);
-                }
-            }
-        });
-
-        pegasus.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    image.setImageResource(R.drawable.mg_3);
-                }
+            public void onClick(View v) {
+                rotate += 10;
+                imageView.setRotation(rotate);
             }
         });
     }
