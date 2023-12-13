8696d75cbcbb5c42d89343eff04b6c106fdae7fe
hsyoodev <hsyoodev@gmail.com>
2023-12-13 11:04:37 +0900

    Java 코드로 화면 만들기

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +12,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        ll.setBackgroundColor(0xff00ff55);
+        Button btn = new Button(this);
+        btn.setText("버튼!!!");
+        ll.addView(btn);
+        setContentView(ll);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = new Button(MainActivity.this);
+                ll.addView(button);
+            }
+        });
     }
 }
