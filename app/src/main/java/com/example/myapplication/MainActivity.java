7b46ad1c23e0254768c2c4dcdf259de7ec1a073e
hsyoodev <hsyoodev@gmail.com>
2023-12-13 11:21:33 +0900

    RelativeLayout 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,21 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        LinearLayout ll = new LinearLayout(this);
-        ll.setOrientation(LinearLayout.VERTICAL);
-        ll.setBackgroundColor(0xff00ff55);
-        Button btn = new Button(this);
-        btn.setText("버튼!!!");
-        ll.addView(btn);
-        setContentView(ll);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button button = new Button(MainActivity.this);
-                ll.addView(button);
-            }
-        });
+        setContentView(R.layout.activity_main);
     }
 }
