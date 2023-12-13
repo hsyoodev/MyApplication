6f24da399bf63e5ffe0c7f5164bafff0e8d0b438
hsyoodev <hsyoodev@gmail.com>
2023-12-13 16:48:38 +0900

    5장 연습문제 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_q1);
+        setContentView(R.layout.activity_q2);
     }
 }
