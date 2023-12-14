eeed1adc2d558739dc23e2a942e98191093662b4
hsyoodev <hsyoodev@gmail.com>
2023-12-14 09:46:50 +0900

    5장 연습문제 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,41 +2,42 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.RadioButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioButton btnA, btnB, btnC;
-    ImageView a, b, c;
-    ImageView aa, bb, cc;
+    LinearLayout a, b, c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnA = findViewById(R.id.btn_a);
-        btnB = findViewById(R.id.btn_b);
-        btnC = findViewById(R.id.btn_c);
         a = findViewById(R.id.a);
         b = findViewById(R.id.b);
         c = findViewById(R.id.c);
-        aa = findViewById(R.id.aa);
-        bb = findViewById(R.id.bb);
-        cc = findViewById(R.id.cc);
 
-        btnA.setOnClickListener(new View.OnClickListener() {
+        a.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                a.setVisibility(View.VISIBLE);
-                aa.setVisibility(View.VISIBLE);
-                b.setVisibility(View.INVISIBLE);
-                bb.setVisibility(View.INVISIBLE);
-                c.setVisibility(View.INVISIBLE);
-                cc.setVisibility(View.INVISIBLE);
+                Toast.makeText(MainActivity.this, String.format("width : %s, height: %s", a.getWidth(), a.getHeight()), Toast.LENGTH_SHORT).show();
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, String.format("width : %s, height: %s", b.getWidth(), b.getHeight()), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, String.format("width : %s, height: %s", c.getWidth(), c.getHeight()), Toast.LENGTH_SHORT).show();
+
             }
         });
 
