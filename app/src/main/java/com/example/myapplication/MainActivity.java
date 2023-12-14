2b62f3598bd45bdc011b924282465fad37f9d1ca
hsyoodev <hsyoodev@gmail.com>
2023-12-14 12:26:10 +0900

    advanced widget 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,84 +1,31 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Chronometer;
-import android.widget.DatePicker;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.TimePicker;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView.CommaTokenizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Chronometer chronometer1;
-    RadioButton rdoCal, rdoTime;
-    DatePicker datePicker1;
-    TimePicker timePicker1;
-    TextView tvYear, tvMonth, tvDay;
-    TextView tvHour, tvMinute;
-    Button start, stop;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        chronometer1 = findViewById(R.id.chronometer1);
-        start = findViewById(R.id.start);
-        stop = findViewById(R.id.stop);
+        String[] items = {"CSI-뉴욕", "CSI-라스베가스", "CSI-마이애미", "Friends",
+                "Fringe", "Lost", "드래곤볼", "원피스"};
 
-        rdoCal = findViewById(R.id.rdoCal);
-        rdoTime = findViewById(R.id.rdoTime);
-        datePicker1 = findViewById(R.id.datePicker1);
-        timePicker1 = findViewById(R.id.timePicker1);
+        AutoCompleteTextView auto = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, items);
+        auto.setAdapter(adapter);
 
-        tvYear = findViewById(R.id.tvYear);
-        tvMonth = findViewById(R.id.tvMonth);
-        tvDay = findViewById(R.id.tvDay);
-        tvHour = findViewById(R.id.tvHour);
-        tvMinute = findViewById(R.id.tvMinute);
-
-        rdoCal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                datePicker1.setVisibility(View.VISIBLE);
-                timePicker1.setVisibility(View.INVISIBLE);
-            }
-        });
-        rdoTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                datePicker1.setVisibility(View.INVISIBLE);
-                timePicker1.setVisibility(View.VISIBLE);
-            }
-        });
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chronometer1.setBase(SystemClock.elapsedRealtime());
-                chronometer1.start();
-            }
-        });
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int year = datePicker1.getYear();
-                int month = datePicker1.getMonth();
-                int day = datePicker1.getDayOfMonth();
-                int hour = timePicker1.getHour();
-                int minute = timePicker1.getMinute();
-                tvYear.setText(String.valueOf(year));
-                tvMonth.setText(String.valueOf(month));
-                tvDay.setText(String.valueOf(day));
-                tvHour.setText(String.valueOf(hour));
-                tvMinute.setText(String.valueOf(minute));
-                chronometer1.stop();
-            }
-        });
-
-
+        MultiAutoCompleteTextView multi = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView1);
+        CommaTokenizer token = new MultiAutoCompleteTextView.CommaTokenizer();
+        multi.setTokenizer(token);
+        multi.setAdapter(adapter);
     }
 }
