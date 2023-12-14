812c01907acd59c06796b5f1cfc5a0dfa7819af5
hsyoodev <hsyoodev@gmail.com>
2023-12-14 10:30:02 +0900

    advanced widget

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,45 +1,13 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    LinearLayout a, b, c;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        a = findViewById(R.id.a);
-        b = findViewById(R.id.b);
-        c = findViewById(R.id.c);
-
-        a.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, String.format("width : %s, height: %s", a.getWidth(), a.getHeight()), Toast.LENGTH_SHORT).show();
-            }
-        });
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, String.format("width : %s, height: %s", b.getWidth(), b.getHeight()), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        c.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, String.format("width : %s, height: %s", c.getWidth(), c.getHeight()), Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
     }
 }
