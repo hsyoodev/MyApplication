8a469f4d8eca8f4903a886a81fb3e1b2815956bb
hsyoodev <hsyoodev@gmail.com>
2023-12-14 15:27:57 +0900

    ViewFlipper

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPrev, btnNext;
+        final ViewFlipper vFlipper;
+
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        vFlipper = (ViewFlipper) findViewById(R.id.viewFlipper1);
+        vFlipper.setFlipInterval(1000);
+        vFlipper.setAutoStart(true);
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                vFlipper.showPrevious();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                vFlipper.showNext();
+            }
+        });
     }
 }
