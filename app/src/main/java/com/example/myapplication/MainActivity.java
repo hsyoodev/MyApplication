128574dcdda4ed0a8993612fb2563fef0b54d2d7
hsyoodev <hsyoodev@gmail.com>
2023-12-14 12:37:14 +0900

    advanced widget 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.MultiAutoCompleteTextView;
-import android.widget.MultiAutoCompleteTextView.CommaTokenizer;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,17 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] items = {"CSI-뉴욕", "CSI-라스베가스", "CSI-마이애미", "Friends",
-                "Fringe", "Lost", "드래곤볼", "원피스"};
-
-        AutoCompleteTextView auto = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, items);
-        auto.setAdapter(adapter);
-
-        MultiAutoCompleteTextView multi = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView1);
-        CommaTokenizer token = new MultiAutoCompleteTextView.CommaTokenizer();
-        multi.setTokenizer(token);
-        multi.setAdapter(adapter);
+        ProgressBar progressBar = findViewById(R.id.progress);
+        SeekBar seekBar = findViewById(R.id.seek);
+        RatingBar rating = findViewById(R.id.rating);
+        progressBar.setProgress(70);
+        seekBar.setProgress(80);
+        rating.setRating(4.5f);
     }
 }
