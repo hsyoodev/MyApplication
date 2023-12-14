01eb5dae3d5cb0098430e0203f5b28a6dbcd9e9e
hsyoodev <hsyoodev@gmail.com>
2023-12-14 13:54:22 +0900

    ScrolleView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.ProgressBar;
-import android.widget.RatingBar;
-import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ProgressBar progressBar = findViewById(R.id.progress);
-        SeekBar seekBar = findViewById(R.id.seek);
-        RatingBar rating = findViewById(R.id.rating);
-        progressBar.setProgress(70);
-        seekBar.setProgress(80);
-        rating.setRating(4.5f);
     }
 }
