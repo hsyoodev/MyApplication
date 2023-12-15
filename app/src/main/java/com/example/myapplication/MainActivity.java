5d333f928bd67a8230a078a553f1e09086d358f8
hsyoodev <hsyoodev@gmail.com>
2023-12-15 11:35:54 +0900

    WebView 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,38 +1,49 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
-    Button cat, dog;
-    FragmentManager fm;
+
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cat = findViewById(R.id.cat);
-        dog = findViewById(R.id.dog);
+        edtUrl = (EditText) findViewById(R.id.edtUrl);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        web = (WebView) findViewById(R.id.webView1);
 
-        fm = getSupportFragmentManager();
-
-        cat.setOnClickListener(new View.OnClickListener() {
+        web.setWebViewClient(new WebViewClient());
+        WebSettings ws = web.getSettings();
+        ws.setJavaScriptEnabled(true);
+        btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                fm.beginTransaction().replace(
-                        R.id.layout, new MyFragment("cat")).commit();
+            public void onClick(View v) {
+                web.loadUrl(edtUrl.getText().toString());
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(edtUrl.getWindowToken(), 0);
             }
         });
-        dog.setOnClickListener(new View.OnClickListener() {
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                fm.beginTransaction().replace(
-                        R.id.layout, new MyFragment("dog")).commit();
+            public void onClick(View v) {
+                web.goBack();
             }
         });
     }
