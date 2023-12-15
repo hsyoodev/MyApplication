5895d4595dab14d131873d50946852f2be0188b8
hsyoodev <hsyoodev@gmail.com>
2023-12-15 16:06:47 +0900

    AlertDialog 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 
 import androidx.appcompat.app.AlertDialog;
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,11 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder ab = new AlertDialog.Builder(MainActivity.this);
                 ab.setTitle("대화상자");
+                ab.setMessage("내용1dsdsdsdsdsdsds");
+                ab.setIcon(R.drawable.ic_launcher_background);
+                ab.setPositiveButton("확인", null);
+                ab.setNeutralButton("중립", null);
+                ab.setNegativeButton("취소", null);
                 ab.show();
             }
         });
