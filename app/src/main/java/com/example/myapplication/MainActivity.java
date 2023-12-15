ffe624b4b67ef7a6d93e5efda06ca04a40c54bd9
hsyoodev <hsyoodev@gmail.com>
2023-12-15 17:24:42 +0900

    파일 읽고 쓰기 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,53 +4,54 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatePicker dp;
+    EditText edtDiary;
+    Button btnWrite;
+    String fileName;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("간단 일기장");
 
-        Button btnRead, btnWrite;
-        btnRead = (Button) findViewById(R.id.btnRead);
+        dp = (DatePicker) findViewById(R.id.datePicker1);
+        edtDiary = (EditText) findViewById(R.id.edtDiary);
         btnWrite = (Button) findViewById(R.id.btnWrite);
 
         btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
+                int year = dp.getYear();
+                int month = dp.getMonth() + 1;
+                int day = dp.getDayOfMonth();
+                String name = String.format("%s년_%s월_%s일", year, month, day);
+                FileOutputStream outFs = null;
                 try {
-                    FileOutputStream outFs = openFileOutput("file.txt",
-                            Context.MODE_PRIVATE);
-                    String str = "쿡북 안드로이드";
-                    outFs.write(str.getBytes());
-                    outFs.close();
-                    Toast.makeText(getApplicationContext(), "file.txt가 생성됨", Toast.LENGTH_SHORT).show();
+                    outFs = openFileOutput(name, Context.MODE_PRIVATE);
+                    outFs.write("쿡북 안드로이드".getBytes());
                 } catch (IOException e) {
-                }
-            }
-        });
-
-        btnRead.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    FileInputStream inFs = openFileInput("file.txt");
-                    byte[] txt = new byte[30];
-                    inFs.read(txt);
-                    String str = new String(txt);
-                    Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
-                    inFs.close();
-                } catch (IOException e) {
-                    Toast.makeText(getApplicationContext(), "파일 없음", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "쿡북 안드로이드", Toast.LENGTH_SHORT).show();
+                } finally {
+                    try {
+                        outFs.close();
+                        Toast.makeText(MainActivity.this, "잘 써짐", Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        Toast.makeText(MainActivity.this, "쓰다 에러남", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
     }
-
 }
