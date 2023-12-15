4b045909e582c1e9151dbfacfa458602849ba178
hsyoodev <hsyoodev@gmail.com>
2023-12-15 11:46:18 +0900

    WebView 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,50 +1,22 @@
 package com.example.myapplication;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText edtUrl;
-    Button btnGo, btnBack;
-    WebView web;
+    WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtUrl = (EditText) findViewById(R.id.edtUrl);
-        btnGo = (Button) findViewById(R.id.btnGo);
-        btnBack = (Button) findViewById(R.id.btnBack);
-        web = (WebView) findViewById(R.id.webView1);
-
-        web.setWebViewClient(new WebViewClient());
-        WebSettings ws = web.getSettings();
+        webView = findViewById(R.id.webview);
+        WebSettings ws = webView.getSettings();
         ws.setJavaScriptEnabled(true);
-        btnGo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                web.loadUrl(edtUrl.getText().toString());
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(edtUrl.getWindowToken(), 0);
-            }
-        });
-
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                web.goBack();
-            }
-        });
+        webView.loadUrl("file:///android_asset/index.html");
     }
 }
