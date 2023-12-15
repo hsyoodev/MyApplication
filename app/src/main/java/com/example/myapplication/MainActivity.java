4ddec4da57a94a0327aed0a198a2818d36877714
hsyoodev <hsyoodev@gmail.com>
2023-12-15 14:02:21 +0900

    menu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,22 +1,55 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    WebView webView;
+    LinearLayout baseLayout;
+    Button button1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("배경색 바꾸기(컨텍스트 메뉴)");
+        baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
+        button1 = (Button) findViewById(R.id.button1);
+    }
 
-        webView = findViewById(R.id.webview);
-        WebSettings ws = webView.getSettings();
-        ws.setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/index.html");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.itemRed) {
+            baseLayout.setBackgroundColor(Color.RED);
+            return true;
+        } else if (itemId == R.id.itemGreen) {
+            baseLayout.setBackgroundColor(Color.GREEN);
+            return true;
+        } else if (itemId == R.id.itemBlue) {
+            baseLayout.setBackgroundColor(Color.BLUE);
+            return true;
+        } else if (itemId == R.id.subRotate) {
+            button1.setRotation(45);
+            return true;
+        } else if (itemId == R.id.subSize) {
+            button1.setScaleX(2);
+            return true;
+        }
+        return false;
     }
 }
