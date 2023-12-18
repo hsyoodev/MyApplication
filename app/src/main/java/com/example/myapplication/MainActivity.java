817b8aa08bb2da5e0443f331d732c31a2108d1dd
hsyoodev <hsyoodev@gmail.com>
2023-12-18 14:41:47 +0900

    sdcard에서 이미지 불러오기

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,98 @@
 package com.example.myapplication;
 
 import android.Manifest;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button prev, next;
+
+    int num = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] per = {
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA
+        imageView = findViewById(R.id.imageView);
+        prev = findViewById(R.id.prev);
+        next = findViewById(R.id.next);
 
-        };
-        ActivityCompat.requestPermissions(this, per, MODE_PRIVATE);
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+        ActivityCompat.requestPermissions(this, permissions, MODE_PRIVATE);
+
+        File f = new File("/sdcard/Pictures");
+        File[] list = f.listFiles();
+        for (File file : list) {
+            Log.e("file", file.getName());
+        }
+
+        File first = list[Math.min(9, num++)];
+        Bitmap bitmap = null;
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            bitmap = BitmapFactory.decodeStream(new FileInputStream(first), null, options);
+        } catch (FileNotFoundException e) {
+            Log.e("error", "에러남");
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (num >= list.length - 1) {
+                    num = 0;
+                }
+                Log.e("num", String.valueOf(num));
+                File first = list[num++];
+                Bitmap bitmap = null;
+                try {
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                    bitmap = BitmapFactory.decodeStream(new FileInputStream(first), null, options);
+                } catch (FileNotFoundException e) {
+                    Log.e("error", "에러남");
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (num < 0) {
+                    num = list.length - 1;
+                }
+                Log.e("num--", String.valueOf(num));
+                File first = list[num--];
+                Bitmap bitmap = null;
+                try {
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                    bitmap = BitmapFactory.decodeStream(new FileInputStream(first), null, options);
+                } catch (FileNotFoundException e) {
+                    Log.e("error", "에러남");
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(bitmap);
+            }
+        });
     }
 }
