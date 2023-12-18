fa58c9c3f44bca4a25ae7f7177901925b5f19a6d
hsyoodev <hsyoodev@gmail.com>
2023-12-18 11:23:53 +0900

    파일 읽고 쓰기 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -10,6 +11,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -30,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
         edtDiary = (EditText) findViewById(R.id.edtDiary);
         btnWrite = (Button) findViewById(R.id.btnWrite);
 
+        dp.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                String name = String.format("%s년_%s월_%s일", year, (monthOfYear + 1), dayOfMonth);
+                try {
+                    FileInputStream inFs = openFileInput(name);
+                    byte[] buf = new byte[4096];
+                    inFs.read(buf);
+                    inFs.close();
+                    String text = new String(buf);
+                    Log.w("text", text);
+                    edtDiary.setText(text);
+                } catch (FileNotFoundException e) {
+                    Log.w("예외", "파일 없음");
+                    edtDiary.setText("");
+                    edtDiary.setHint("파일 없음, 내용을 작성해주세요.");
+                } catch (IOException e) {
+                    Log.w("IOException", "error");
+                }
+            }
+        });
+
         btnWrite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     outFs = openFileOutput(name, Context.MODE_PRIVATE);
                     outFs.write("쿡북 안드로이드".getBytes());
+                    outFs.write(edtDiary.getText().toString().getBytes());
                 } catch (IOException e) {
                     Toast.makeText(MainActivity.this, "쿡북 안드로이드", Toast.LENGTH_SHORT).show();
                 } finally {
