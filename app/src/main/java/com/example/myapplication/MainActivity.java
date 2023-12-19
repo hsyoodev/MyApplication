d1e93396ccb92759c08ee3a493f9cff18aa9f4ac
hsyoodev <hsyoodev@gmail.com>
2023-12-19 11:15:31 +0900

    new activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,107 +1,28 @@
 package com.example.myapplication;
 
-import android.Manifest;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    Button prev, next;
-
-    TextView textView;
-
-    int num = 0;
+    Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
-        prev = findViewById(R.id.prev);
-        next = findViewById(R.id.next);
-        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
 
-        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-        ActivityCompat.requestPermissions(this, permissions, MODE_PRIVATE);
-
-        File f = new File("/sdcard/Pictures");
-        File[] list = f.listFiles();
-        for (File file : list) {
-            Log.e("file", file.getName());
-        }
-
-        File first = list[Math.min(9, num++)];
-        Bitmap bitmap = null;
-        try {
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-            bitmap = BitmapFactory.decodeStream(new FileInputStream(first), null, options);
-        } catch (FileNotFoundException e) {
-            Log.e("error", "에러남");
-            e.printStackTrace();
-        }
-        imageView.setImageBitmap(bitmap);
-
-        textView.setText(String.format("%s / %s", num, list.length - 1));
-
-        next.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (num >= list.length - 1) {
-                    num = 0;
-                }
-                Log.e("num", String.valueOf(num));
-                File first = list[num++];
-                Bitmap bitmap = null;
-                try {
-                    BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-                    bitmap = BitmapFactory.decodeStream(new FileInputStream(first), null, options);
-                } catch (FileNotFoundException e) {
-                    Log.e("error", "에러남");
-                    e.printStackTrace();
-                }
-                imageView.setImageBitmap(bitmap);
-
-                textView.setText(String.format("%s / %s", num, list.length - 1));
-            }
-        });
-
-        prev.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (num < 0) {
-                    num = list.length - 1;
-                }
-                Log.e("num--", String.valueOf(num));
-                File first = list[num--];
-                Bitmap bitmap = null;
-                try {
-                    BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-                    bitmap = BitmapFactory.decodeStream(new FileInputStream(first), null, options);
-                } catch (FileNotFoundException e) {
-                    Log.e("error", "에러남");
-                    e.printStackTrace();
-                }
-                imageView.setImageBitmap(bitmap);
-
-                textView.setText(String.format("%s / %s", num, list.length - 1));
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
             }
         });
     }
