14e61f1ffb57913795e7a4eb85cb7ea09de274f1
hsyoodev <hsyoodev@gmail.com>
2023-12-19 12:38:40 +0900

    10ìcheck에 따라서 화면이동

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,42 +1,39 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button, button2;
+    RadioButton second, third;
+    Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        second = findViewById(R.id.second);
+        third = findViewById(R.id.third);
         button = findViewById(R.id.button);
-        button2 = findViewById(R.id.button2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 명시적 인텐트
-                Uri uri = Uri.parse("http://ggoreb.com");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 암시적 인텐트
-                Uri uri = Uri.parse("tel:123456789");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+                Intent intent = null;
+                if (second.isChecked()) {
+                    intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                }
+                if (third.isChecked()) {
+                    intent = new Intent(MainActivity.this, MainActivity3.class);
+                    startActivity(intent);
+                }
             }
         });
     }
