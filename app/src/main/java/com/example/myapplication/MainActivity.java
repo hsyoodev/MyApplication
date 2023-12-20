20f267bbca04b9c655889f4686e929a5be558ef5
hsyoodev <hsyoodev@gmail.com>
2023-12-20 13:49:52 +0900

    카메라 데이터 전달

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,61 +1,45 @@
 package com.example.myapplication;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    String operation;
+public class MainActivity extends AppCompatActivity {
+
+    Button btnPhoto;
+    ImageView imageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("메인 액티비티");
 
-        RadioGroup radioGroup = findViewById(R.id.radioGroup);
-        Button btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
+        btnPhoto = findViewById(R.id.btnPhoto);
+        imageView = findViewById(R.id.imageView);
 
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton = findViewById(checkedId);
-                operation = radioButton.getText().toString();
-            }
-        });
-        btnNewActivity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-                EditText edtNum1 = (EditText) findViewById(R.id.edtNum1);
-                EditText edtNum2 = (EditText) findViewById(R.id.edtNum2);
-
-                Intent intent = new Intent(getApplicationContext(),
-                        SecondActivity.class);
-                intent.putExtra("Num1",
-                        Integer.parseInt(edtNum1.getText().toString()));
-                intent.putExtra("Num2",
-                        Integer.parseInt(edtNum2.getText().toString()));
-                intent.putExtra("operation",
-                        operation);
-                startActivityForResult(intent, 0);
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i, 0);
             }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK) {
-            int hap = data.getIntExtra("Hap", 0);
-            Toast.makeText(getApplicationContext(), "합계 :" + hap,
-                    Toast.LENGTH_SHORT).show();
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
         }
     }
 }
