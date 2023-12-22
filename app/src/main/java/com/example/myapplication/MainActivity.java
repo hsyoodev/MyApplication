31e1b5ccffb9e9b0ccfdcc696e46bbf04c211a29
hsyoodev <hsyoodev@gmail.com>
2023-12-22 17:33:13 +0900

    SQLite

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,52 +1,47 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Spinner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("스피너 테스트");
-
-        final String[] movie = {"쿵푸팬더", "짱구는 못말려", "아저씨", "아바타", "대부", "국가대표",
-                "토이스토리3", "마당을 나온 암탉", "죽은 시인의 사회", "서유기"};
-        final int[] posters = {R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05, R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10};
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
-        ArrayAdapter<String> adapter;
-        adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, movie);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                ImageView imageView = findViewById(R.id.image_view);
-                imageView.setImageResource(posters[position]);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        Button button = findViewById(R.id.q2);
+        myDBHelper helper = new myDBHelper(this);
+        Button button = findViewById(R.id.btnInsert);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                startActivity(intent);
+                SQLiteDatabase db = helper.getWritableDatabase();
+                db.execSQL("insert into groupDB values ('BTS', 9);");
+                db.close();
             }
         });
+    }
+
+    public class myDBHelper extends SQLiteOpenHelper {
+
+        public myDBHelper(@Nullable Context context) {
+            super(context, "groupDB.db", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE groupDB (gName char(20) primary key, gNumber integer);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
     }
 }
