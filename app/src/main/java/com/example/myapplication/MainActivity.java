d087790f8e43afa14d66fc3e270c71681e197d8f
hsyoodev <hsyoodev@gmail.com>
2023-12-26 17:02:25 +0900

    SharedPreferences 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sp;
     EditText editId;
     EditText editName;
     CheckBox checkAgree;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         editName = (EditText) findViewById(R.id.edit_name);
         checkAgree = (CheckBox) findViewById(R.id.check_agree);
         sp = getSharedPreferences("myData", MODE_PRIVATE);
+        textView = findViewById(R.id.date);
     }
 
     @Override
@@ -29,9 +35,11 @@ public class MainActivity extends AppCompatActivity {
         String id = sp.getString("id", "");
         String name = sp.getString("name", "");
         boolean isAgree = sp.getBoolean("isAgree", false);
+        String date = sp.getString("date", "");
         editId.setText(id);
         editName.setText(name);
         checkAgree.setChecked(isAgree);
+        textView.setText(date);
     }
 
     @Override
@@ -44,6 +52,16 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("id", id);
         editor.putString("name", name);
         editor.putBoolean("isAgree", isAgree);
+        editor.commit();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Date now = new Date();
+        SharedPreferences.Editor editor = sp.edit();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY년 MM월 dd일 HH시 mm분 ss초");
+        editor.putString("date", simpleDateFormat.format(now));
         editor.commit();
     }
 }
