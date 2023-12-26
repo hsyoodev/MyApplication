e21c08fb0e43554093d77687fc3abe7a383cd6f4
hsyoodev <hsyoodev@gmail.com>
2023-12-26 13:45:32 +0900

    11장 연습문제

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,70 +5,63 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-    List<Group> list = new ArrayList<>();
+    TextView lastLoginEndDate;
+    SQLiteDatabase db;
+    myDBHelper helper = new myDBHelper(this);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myDBHelper helper = new myDBHelper(this);
-        Button btnInsert = findViewById(R.id.btnInsert);
-        EditText edtName = findViewById(R.id.edtName);
-        EditText edtNumber = findViewById(R.id.edtNumber);
-        Button btnSelect = findViewById(R.id.btnSelect);
-        ListView listView = findViewById(R.id.list_view);
-        btnInsert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SQLiteDatabase db = helper.getWritableDatabase();
-                db.execSQL("insert into groupDB values ('" +
-                        edtName.getText().toString() +
-                        "', " + edtNumber.getText().toString() + ");");
-                db.close();
-            }
-        });
+        db = helper.getReadableDatabase();
+        lastLoginEndDate = findViewById(R.id.last_login_end_date);
+        Cursor cursor = db.rawQuery("SELECT id, date_time FROM history ORDER BY date_time DESC", null);
+        String dateTime = "";
+        while (cursor.moveToNext()) {
+            dateTime = cursor.getString(1);
+            break;
+        }
+        lastLoginEndDate.setText(dateTime);
+    }
 
-        btnSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                list.clear();
-                SQLiteDatabase db = helper.getWritableDatabase();
-                Cursor cursor = db.rawQuery("select gName, gNumber from groupDB", null);
-                while (cursor.moveToNext()) {
-                    String gName = cursor.getString(0);
-                    int gNumber = cursor.getInt(1);
-                    Group group = new Group(gName, gNumber);
-                    list.add(group);
-                }
-                CustomAdapter customAdapter = new CustomAdapter(MainActivity.this, R.layout.item, list);
-                listView.setAdapter(customAdapter);
-                cursor.close();
-                db.close();
-            }
-        });
+    @Override
+    protected void onResume() {
+        super.onResume();
+        db = helper.getReadableDatabase();
+        lastLoginEndDate = findViewById(R.id.last_login_end_date);
+        Cursor cursor = db.rawQuery("SELECT id, date_time FROM history ORDER BY date_time DESC", null);
+        String dateTime = "";
+        while (cursor.moveToNext()) {
+            dateTime = cursor.getString(1);
+            break;
+        }
+        lastLoginEndDate.setText(dateTime);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db = helper.getWritableDatabase();
+        db.execSQL("INSERT INTO history (id, date_time) VALUES (NULL, (datetime('now', 'localtime')))");
     }
 
     public class myDBHelper extends SQLiteOpenHelper {
 
         public myDBHelper(@Nullable Context context) {
-            super(context, "groupDB.db", null, 1);
+            super(context, "history.db", null, 1);
         }
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE groupDB (gName char(20) primary key, gNumber integer);");
+            db.execSQL("CREATE TABLE history (\n" +
+                    "    id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                    "    date_time TEXT);");
         }
 
         @Override
