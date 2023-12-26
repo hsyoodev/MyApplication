e0de8aa167f7aaca8aaca33d7de8855757b82fda
hsyoodev <hsyoodev@gmail.com>
2023-12-26 15:16:33 +0900

    sdcard안에다가 db 넣는 방법 asset

---
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -0,0 +1,117 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private final String dbName = "data.db";
+
+    public DBHelper(Context context) {
+        super(context, "data.db", null, 1);
+        if (!isExistDB(context)) { // 앱 처음 실행 (DB 데이터 없음)
+            copyDB(context); // DB 데이터 생성 (복사)
+        }
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    public boolean isExistDB(Context context) {
+        String packageName = context.getPackageName();
+        String filePath = "/data/data/" + packageName + "/databases/" + dbName;
+        File file = new File(filePath);
+        if (file.exists()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void copyDB(Context context) {
+        AssetManager manager = context.getAssets();
+        String packageName = context.getPackageName();
+        String folderPath = "/data/data/" + packageName + "/databases";
+        File folder = new File(folderPath);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+        InputStream is = null;
+        BufferedInputStream bis = null;
+        OutputStream fos = null;
+        BufferedOutputStream bos = null;
+        try {
+            is = manager.open(dbName);
+            bis = new BufferedInputStream(is);
+            fos = new FileOutputStream(folderPath + "/" + dbName);
+            bos = new BufferedOutputStream(fos);
+            int data = 0;
+            while ((data = bis.read()) != -1) {
+                bos.write(data);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                }
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                }
+            }
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    public List<Map<String, Object>> select() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor =
+                db.rawQuery("SELECT empno, ename, job, hiredate, deptno FROM emp", null);
+        while (cursor.moveToNext()) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("empno", cursor.getInt(0));
+            map.put("ename", cursor.getString(1));
+            map.put("job", cursor.getString(2));
+            map.put("hiredate", cursor.getString(3));
+            map.put("deptno", cursor.getInt(4));
+            list.add(map);
+        }
+        db.close();
+        return list;
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,71 +1,33 @@
 package com.example.myapplication;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.Manifest;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    TextView lastLoginEndDate;
-    SQLiteDatabase db;
-    myDBHelper helper = new myDBHelper(this);
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        helper.select();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        helper.select();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        helper.insert();
-    }
-
-    public class myDBHelper extends SQLiteOpenHelper {
-
-        public myDBHelper(@Nullable Context context) {
-            super(context, "history.db", null, 1);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE history (\n" +
-                    "    id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
-                    "    date_time TEXT);");
-        }
-
-        public void select() {
-            db = getReadableDatabase();
-            lastLoginEndDate = findViewById(R.id.last_login_end_date);
-            Cursor cursor = db.rawQuery("SELECT id, date_time FROM history ORDER BY date_time DESC", null);
-            if (cursor.moveToNext()) {
-                lastLoginEndDate.setText(cursor.getString(1));
-            }
-            db.close();
-        }
-
-        public void insert() {
-            db = getWritableDatabase();
-            db.execSQL("INSERT INTO history (id, date_time) VALUES (NULL, (datetime('now', 'localtime')))");
-            db.close();
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        String[] per = {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+        ActivityCompat.requestPermissions(this, per, MODE_PRIVATE);
+        
+        TextView content = (TextView) findViewById(R.id.content);
+        DBHelper dbHelper = new DBHelper(this);
+        List<Map<String, Object>> list = dbHelper.select();
+        for (int i = 0; i < list.size(); i++) {
+            content.append("===========================\n");
+            content.append(list.get(i).toString() + "\n");
         }
     }
 }
