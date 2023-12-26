68c90a08f88926915eaa9ae6b6206844aafc5e24
hsyoodev <hsyoodev@gmail.com>
2023-12-26 11:10:35 +0900

    SQLite read 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         EditText edtName = findViewById(R.id.edtName);
         EditText edtNumber = findViewById(R.id.edtNumber);
         Button btnSelect = findViewById(R.id.btnSelect);
+        EditText edtNameResult = findViewById(R.id.edtNameResult);
+        EditText edtNumberResult = findViewById(R.id.edtNumberResult);
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,11 +41,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 SQLiteDatabase db = helper.getWritableDatabase();
                 Cursor cursor = db.rawQuery("select gName, gNumber from groupDB", null);
+                String gName = "";
+                String gNumber = "";
                 while (cursor.moveToNext()) {
-                    String gName = cursor.getString(0);
-                    int gNumber = cursor.getInt(1);
-                    Log.e("조회", gName + gNumber);
+                    gName += cursor.getString(0) + "\n";
+                    gNumber += cursor.getString(1) + "\n";
                 }
+                edtNameResult.setText(gName);
+                edtNumberResult.setText(gNumber);
                 cursor.close();
                 db.close();
             }
