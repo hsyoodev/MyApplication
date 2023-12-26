22ae5f0eea946487d3ad5a101d74999670c918e4
hsyoodev <hsyoodev@gmail.com>
2023-12-26 10:46:29 +0900

    SQLite insert, read

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,12 +20,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDBHelper helper = new myDBHelper(this);
-        Button button = findViewById(R.id.btnInsert);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button btnInsert = findViewById(R.id.btnInsert);
+        EditText edtName = findViewById(R.id.edtName);
+        EditText edtNumber = findViewById(R.id.edtNumber);
+        Button btnSelect = findViewById(R.id.btnSelect);
+        btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SQLiteDatabase db = helper.getWritableDatabase();
-                db.execSQL("insert into groupDB values ('BTS', 9);");
+                db.execSQL("insert into groupDB values ('" +
+                        edtName.getText().toString() +
+                        "', " + edtNumber.getText().toString() + ");");
+                db.close();
+            }
+        });
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = helper.getWritableDatabase();
+                Cursor cursor = db.rawQuery("select gName, gNumber from groupDB", null);
+                while (cursor.moveToNext()) {
+                    String gName = cursor.getString(0);
+                    int gNumber = cursor.getInt(1);
+                    Log.e("조회", gName + gNumber);
+                }
+                cursor.close();
                 db.close();
             }
         });
