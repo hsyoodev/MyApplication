ec3456621c404f03eb82ef09b8f329eadcb10ade
hsyoodev <hsyoodev@gmail.com>
2023-12-26 16:05:29 +0900

    SharedPreferences

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,33 +1,49 @@
 package com.example.myapplication;
 
-import android.Manifest;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sp;
+    EditText editId;
+    EditText editName;
+    CheckBox checkAgree;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editId = (EditText) findViewById(R.id.edit_id);
+        editName = (EditText) findViewById(R.id.edit_name);
+        checkAgree = (CheckBox) findViewById(R.id.check_agree);
+        sp = getSharedPreferences("myData", MODE_PRIVATE);
+    }
 
-        String[] per = {
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-        };
-        ActivityCompat.requestPermissions(this, per, MODE_PRIVATE);
-        
-        TextView content = (TextView) findViewById(R.id.content);
-        DBHelper dbHelper = new DBHelper(this);
-        List<Map<String, Object>> list = dbHelper.select();
-        for (int i = 0; i < list.size(); i++) {
-            content.append("===========================\n");
-            content.append(list.get(i).toString() + "\n");
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String id = sp.getString("id", "");
+        String name = sp.getString("name", "");
+        boolean isAgree = sp.getBoolean("isAgree", false);
+        editId.setText(id);
+        editName.setText(name);
+        checkAgree.setChecked(isAgree);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        String id = editId.getText().toString();
+        String name = editName.getText().toString();
+        boolean isAgree = checkAgree.isChecked();
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("id", id);
+        editor.putString("name", name);
+        editor.putBoolean("isAgree", isAgree);
+        editor.commit();
     }
 }
