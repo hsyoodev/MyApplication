27e342a048d021e8be7e6bb5f1effd7fe46ea6b3
hsyoodev <hsyoodev@gmail.com>
2023-12-27 17:23:54 +0900

    kakaomap sdk 에러

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,153 +1,48 @@
 package com.example.myapplication;
 
-import android.media.MediaPlayer;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.SystemClock;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SeekBar;
-import android.widget.TextView;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
-
-    ListView listViewMP3;
-    Button btnPlay, btnStop;
-
-    // 수정 부분
-    TextView tvMP3;
-    TextView tvTime;
-    SeekBar pbMP3;
-
-    ArrayList<String> mp3List;
-    String selectedMP3;
-
-    String mp3Path = Environment.getExternalStorageDirectory().getPath() + "/";
-    MediaPlayer mPlayer;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("간단 MP3 플레이어");
-        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
 
-        // SDCard의 파일을 읽어서 리스트뷰에 출력
-        mp3List = new ArrayList<String>(); // 가변적 문자열
+        getAppKeyHash();
 
-        File[] listFiles = new File(mp3Path).listFiles();
-        String fileName, extName;
-        for (File file : listFiles) {
-            fileName = file.getName();
-            extName = fileName.substring(fileName.length() - 3);
-            if (extName.equals((String) "mp3")) // 확장명이 mp3일 때만 추가함.
-                mp3List.add(fileName);
-        }
+        MapView mapView = new MapView(this);
 
-        ListView listViewMP3 = (ListView) findViewById(R.id.listViewMP3);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_single_choice, mp3List);
-        listViewMP3.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        listViewMP3.setAdapter(adapter);
-        listViewMP3.setItemChecked(0, true);
-
-        listViewMP3.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> arg0, View arg1,
-                                    int arg2, long arg3) {
-                selectedMP3 = mp3List.get(arg2);
-            }
-        });
-        selectedMP3 = mp3List.get(0);
-
-        btnPlay = (Button) findViewById(R.id.btnPlay);
-        btnStop = (Button) findViewById(R.id.btnStop);
-        tvMP3 = (TextView) findViewById(R.id.tvMP3);
-        pbMP3 = (SeekBar) findViewById(R.id.pbMP3);
-
-        // 이부분 수정
-        tvTime = (TextView) findViewById(R.id.tvTime);
-
-        btnPlay.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    mPlayer = new MediaPlayer();
-                    mPlayer.setDataSource(mp3Path + selectedMP3);
-                    mPlayer.prepare();
-                    mPlayer.start();
-                    btnPlay.setClickable(false);
-                    btnStop.setClickable(true);
-                    tvMP3.setText("실행중인 음악 :  " + selectedMP3);
-
-                    new Thread() {
-                        SimpleDateFormat timeFormat = new SimpleDateFormat(
-                                "mm:ss");
-
-                        public void run() {
-                            if (mPlayer == null)
-                                return;
-                            pbMP3.setMax(mPlayer.getDuration());
-                            while (mPlayer.isPlaying()) {
-                                runOnUiThread(new Runnable() {
-                                    public void run() {
-                                        pbMP3.setProgress(mPlayer
-                                                .getCurrentPosition());
-                                        tvTime.setText("진행 시간 : "
-                                                + timeFormat.format(mPlayer
-                                                .getCurrentPosition()));
-                                    }
-                                });
-                                SystemClock.sleep(200);
-                            }
-                        }
-                    }.start();
-
-                } catch (IOException e) {
-                }
-            }
-        });
-
-        btnStop.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                mPlayer.stop();
-                mPlayer.reset();
-                btnPlay.setClickable(true);
-                btnStop.setClickable(false);
-                tvMP3.setText("실행중인 음악 :  ");
-
-                pbMP3.setProgress(0); // 현재 위치를 지정
-                tvTime.setText("진행 시간 : ");
-            }
-        });
-        btnStop.setClickable(false);
-
-        pbMP3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (fromUser) {
-                    mPlayer.seekTo(progress);
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
+
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info =
+                    getPackageManager().getPackageInfo(
+                            getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            Log.e("name not found", e.toString());
+        }
+    }
+
 }
