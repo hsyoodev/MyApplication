e302fc1aaf4454cdbb347ad184fa4db4329a3bc1
hsyoodev <hsyoodev@gmail.com>
2023-12-28 10:34:51 +0900

    BroadcastReceiver Battery Check

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,39 +1,78 @@
 package com.example.myapplication;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Intent intent;
-    Button btnStart, btnStop;
+    ImageView ivBattery;
+    EditText edtBattery;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("서비스 테스트 예제");
+        setTitle("배터리 상태 체크");
 
-        intent = new Intent(this, MusicService.class);
-        btnStart = (Button) findViewById(R.id.btnStart);
-        btnStop = (Button) findViewById(R.id.btnStop);
+        ivBattery = (ImageView) findViewById(R.id.ivBattery);
+        edtBattery = (EditText) findViewById(R.id.edtBattery);
 
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startService(intent);
-                android.util.Log.i("서비스 테스트", "startService()");
-            }
-        });
-
-        btnStop.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                stopService(intent);
-                android.util.Log.i("서비스 테스트", "stopService()");
-            }
-        });
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(br);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter iFilter = new IntentFilter();
+        iFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(br, iFilter);
+    }
+
+    BroadcastReceiver br = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                int remain = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                edtBattery.setText("현재 충전량 : " + remain + "\n");
+
+                if (remain >= 90)
+                    ivBattery.setImageResource(R.drawable.battery_100);
+                else if (remain >= 70)
+                    ivBattery.setImageResource(R.drawable.battery_80);
+                else if (remain >= 50)
+                    ivBattery.setImageResource(R.drawable.battery_60);
+                else if (remain >= 10)
+                    ivBattery.setImageResource(R.drawable.battery_20);
+                else
+                    ivBattery.setImageResource(R.drawable.battery_0);
+
+                int plug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
+                switch (plug) {
+                    case 0:
+                        edtBattery.append("전원 연결 : 안됨");
+                        break;
+                    case BatteryManager.BATTERY_PLUGGED_AC:
+                        edtBattery.append("전원 연결 : 어댑터 연결됨");
+                        break;
+                    case BatteryManager.BATTERY_PLUGGED_USB:
+                        edtBattery.append("전원 연결 : USB 연결됨");
+                        break;
+                }
+            }
+        }
+    };
 }
