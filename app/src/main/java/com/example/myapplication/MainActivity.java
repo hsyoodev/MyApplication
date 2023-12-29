efb9d1a23b84f3a8b2c14d19b2d216293276eb35
hsyoodev <hsyoodev@gmail.com>
2023-12-29 15:45:39 +0900

    Handler 4 ex) Stop Watch

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.SystemClock;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,16 +13,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    long basetime;
+    long remaintime;
     TextView text;
-    Button button;
-    int count = 0;
+    Button start, stop;
+    boolean isRunning = false;
     Handler handler = new Handler(Looper.getMainLooper()) {
         @Override
         public void handleMessage(@NonNull Message msg) {
-            if (count < 10) {
-                SystemClock.sleep(1000);
-                count++;
-                text.setText(count + "");
+            String time = getTime();
+            text.setText(time);
+            if (isRunning) {
                 handler.sendEmptyMessage(0);
             }
         }
@@ -33,7 +35,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         text = findViewById(R.id.text);
-        button = findViewById(R.id.button);
-        handler.sendEmptyMessage(0);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                basetime = SystemClock.elapsedRealtime();
+                isRunning = true;
+                handler.sendEmptyMessage(0);
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = false;
+            }
+        });
+    }
+
+    private String getTime() {
+        long now = SystemClock.elapsedRealtime();
+        long remain = now + remaintime - basetime;
+        if (!isRunning) {
+            remaintime = remain;
+        }
+        long min = remain / 1000 / 60;
+        long sec = (remain / 1000) % 60;
+        long milliSec = (remain % 1000) / 10; // 소수점 둘째 자리까지 표시
+        String timeFormat = String.format("%02d:%02d:%02d", min, sec, milliSec);
+        return timeFormat;
     }
 }
