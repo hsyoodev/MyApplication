8f664a07b7ffb01aaa65ced10f4ea11607ab1fd3
hsyoodev <hsyoodev@gmail.com>
2023-12-29 14:41:50 +0900

    Handler 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.SystemClock;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,11 +12,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
+    Button button;
     int count = 0;
     Handler handler = new Handler(Looper.getMainLooper()) {
         @Override
         public void handleMessage(@NonNull Message msg) {
-            Log.e("handler", "message");
+            if (count < 10) {
+                SystemClock.sleep(1000);
+                count++;
+                text.setText(count + "");
+                handler.sendEmptyMessage(0);
+            }
         }
     };
 
@@ -27,28 +32,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView text = findViewById(R.id.text);
-        Button button = findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        for (int i = 0; i <= 100; i++) {
-                            final int i2 = i;
-                            SystemClock.sleep(100);
-                            handler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    text.setText(i2 + "");
-                                }
-                            });
-                        }
-                    }
-                }).start();
-            }
-        });
+        text = findViewById(R.id.text);
+        button = findViewById(R.id.button);
+        handler.sendEmptyMessage(0);
     }
 }
