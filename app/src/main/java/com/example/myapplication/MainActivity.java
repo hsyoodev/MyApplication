35c35dae47dc14162bb84bf1f36c41cc5d2d2826
hsyoodev <hsyoodev@gmail.com>
2023-12-29 14:05:57 +0900

    Handler 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,58 +1,38 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.ListView;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity {
+    Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            Log.e("handler", "message");
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Thread(new Runnable() {
+        TextView text = findViewById(R.id.text);
+        Button button = findViewById(R.id.button);
+        
+        handler.sendEmptyMessage(0);
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                URL url = null;
-                try {
-                    url = new URL("http://ggoreb.com/api/youtube.jsp");
-                    URLConnection conn = url.openConnection();
-                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    String json = "";
-                    while (true) {
-                        String line = br.readLine();
-                        if (line == null) {
-                            break;
-                        }
-                        json += line;
-                    }
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    Map<String, Object> youtubes = objectMapper.readValue(json, Map.class);
-                    List<Map<String, Object>> items = (List<Map<String, Object>>) youtubes.get("items");
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            ListView listView = findViewById(R.id.list_view);
-                            CustomAdapter customAdapter = new CustomAdapter(MainActivity.this, R.layout.item, items);
-                            listView.setAdapter(customAdapter);
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                Log.e("post", "post");
             }
-        }).start();
+        }, 6000);
     }
 }
